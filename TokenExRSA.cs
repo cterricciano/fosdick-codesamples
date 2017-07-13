a0f6fcbe5a967344760d2fb12fe63ea7f7798f47
cterricciano <cterricciano@fosdickcorp.com>
2017-07-13 13:11:18 -0400

    Add files via upload

---
+++ TokenExRSA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto.Engines;
+
+namespace encryption_tester
+{
+    public static class TokenExRSA
+    {
+        public static AsymmetricKeyParameter ReadAsymmetricKeyParameter(string pemFilename)
+        {
+            var fileStream = System.IO.File.OpenText(pemFilename);
+            var pemReader = new Org.BouncyCastle.OpenSsl.PemReader(fileStream);
+            var KeyParameter = (Org.BouncyCastle.Crypto.AsymmetricKeyParameter)pemReader.ReadObject();
+            return KeyParameter;
+        }
+        public static string RSAEncryptToBase64(AsymmetricKeyParameter public_key, string text_to_encrypt)
+        {
+            var bytesToEncrypt = Encoding.UTF8.GetBytes(text_to_encrypt);
+            var encryptEngine = new Pkcs1Encoding(new RsaEngine());
+            encryptEngine.Init(true, public_key);
+            return Convert.ToBase64String(encryptEngine.ProcessBlock(bytesToEncrypt, 0, bytesToEncrypt.Length));
+
+        }
+    }
+}
